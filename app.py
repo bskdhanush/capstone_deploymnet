673218884ffcb6d0db734f38db1f0c15e013f86a
bsk dhanush <54901177+bskdhanush@users.noreply.github.com>
2022-12-01 10:10:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template,request
+import model as m
+app=Flask(__name__)
+
+
+@app.route('/',methods=["GET","POST"])
+def home():
+ if request.method == "POST":
+    sg= request.form["sg"]
+    al = request.form["al"]
+    sod = request.form["sod"]
+    hemo = request.form["hemo"]
+    pcv = request.form["pcv"]
+    htn = request.form["htn"]
+    dm = request.form["dm"]
+    pred=m.predection(float(sg),float(al),float(sod),float(hemo),float(pcv),float(htn),float(dm))   
+    if pred==1:
+     return render_template("index.html",pr="Ckd") 
+    else :
+     return render_template("index.html",pr="NCKD")  
+ return render_template("index.html")
+
+ 
+if __name__=="__main__":
+  app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,32 @@
+from keras.models import Sequential, load_model
+import numpy as np
+import pandas as pd
+from keras.layers import Dense
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report,accuracy_score
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler,StandardScaler
+import keras as k 
+import tensorflow as tf
+from sklearn.tree import DecisionTreeClassifier 
+import joblib 
+import pickle
+sc=StandardScaler()
+def predection(sg,al,sod,hemo,pcv,htn,dm):
+ #For ckd data=(3,4,19,49,24,0,0)
+ #For non ckd(0.50,0.8,0.393939,0.412281,0.609756,1.0,0.0 )
+  data=(sg,al,sod,hemo,pcv,htn,dm)
+  data=np.array(data)
+  data=data.reshape((1,-1))
+  print(data)
+ # data=sc.fit_transform(data)
+  print(data)
+  filename='C:\Anaconda\envs\capstone\deployment\CKD_pred.pkl'
+  #loaded_model=k.models.load_model('C:\Anaconda\envs\capstone\deployment\ckd_model.h5')
+  loaded_model=pickle.load(open(filename,'rb'))
+  #loaded_model=joblib.load(filename)
+  data=loaded_model.predict(data)  
+  return data
+
+#x=predection(3,4,19,49,24,0,0)
+#print(x)
+#sg,al,sod,hemo,pcv,htn,dm
